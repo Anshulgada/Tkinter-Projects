c10dd342a4d89591acda3ef24f318f615a290514
Anshul <anshulgada05@gmail.com>
2023-04-10 02:36:44 +0530

    New files

---
+++ Arguments.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+from tkinter import ttk
+
+# def btn_func(entry_str):
+#     print('Button was pressed')
+#     print(entry_str.get())
+
+def outer_func(parameter):
+    def inner_func():
+        print('Button was pressed')
+        print(parameter.get())
+    return inner_func
+
+# Create a window
+window = tk.Tk()
+window.title('') 
+window.geometry('800x500')
+
+# Entry
+entry_str = tk.StringVar(value = 'Test')
+entry = ttk.Entry(window, textvariable = entry_str)
+entry.pack()
+
+# Button
+# btn = ttk.Button(window, text = 'A button', command = lambda: btn_func(entry_str))   # By adding in lambda we make it 
+# btn.pack()                                                                           # so that the func is not called
+
+btn = ttk.Button(window, text = 'A button', command = outer_func(entry_str))  # To directly call, use nested func 
+btn.pack()   
+
+# Run
+window.mainloop()
+print('\n----- Closed mainloop window -----')
--- Buttons.py
+++ Buttons.py
@@ -6,52 +6,87 @@ window = tk.Tk()
 window.title('Buttons')
 window.geometry('600x400')
 
-def btn_func():
-	print('Basic Btn')
-	print(radio_var.get())
+# def btn_func():
+#     print('Basic Btn')
+#     print(radio_var.get())
 
-# Button
-btn_str = tk.StringVar(value = 'A btn with string var')
-btn = ttk.Button(window, text = 'A button', command = btn_func, textvariable = btn_str)
-btn.pack()
+# # Button
+# btn_str = tk.StringVar(value = 'A btn with string var')
+# btn = ttk.Button(window, text = 'A button', command = btn_func, textvariable = btn_str)
+# btn.pack()
 
-# Check Button
-check_var1 = tk.IntVar(value = 10)      # Start Value is 10   # Can be StringVar / BooleanVar also
-check2 = ttk.Checkbutton(
-	window,
-	text = 'Checkbox 1',
-	command = lambda: print(check_var1.get()),
-	variable = check_var1,
-	onvalue = 10,
-	offvalue = 5)
-check2.pack()
+# # Check Button
+# check_var1 = tk.IntVar(value = 10)      # Start Value is 10   # Can be StringVar / BooleanVar also
+# check2 = ttk.Checkbutton(
+#     window,
+#     text = 'Checkbox 1',
+#     command = lambda: print(check_var1.get()),
+#     variable = check_var1,
+#     onvalue = 10,
+#     offvalue = 5)
+# check2.pack()
 
-check2 = ttk.Checkbutton(
-	window,
-	text = 'Checkbox 2',
-	command = lambda: print())
-check2.pack()
+# check2 = ttk.Checkbutton(
+#     window,
+#     text = 'Checkbox 2',
+#     command = lambda: print())
+# check2.pack()
 
+
+# # Radio Buttons
+# radio_var = tk.StringVar()
+# radio1 = ttk.Radiobutton(
+#     window,
+#     text = 'Radio Btn 1',
+#     value = 'Radio 1',
+#     variable = radio_var,
+#     command = lambda: print(radio_var.get()))
+# radio1.pack()
+
+# radio2 = ttk.Radiobutton(
+#     window,
+#     text = 'Radio Btn 2',
+#     value = 2,
+#     variable = radio_var,
+#     command = lambda: print(radio_var.get()))
+# radio2.pack()
+
+
+
+# Exercise ------------
+    
+def rad_func():
+    print(check_bool.get())
+    check_bool.set(False)
+    
 
 # Radio Buttons
 radio_var = tk.StringVar()
 radio1 = ttk.Radiobutton(
-	window,
-	text = 'Radio Btn 1',
-	value = 'Radio 1',
-	variable = radio_var,
-	command = lambda: print(radio_var.get()))
+    window,
+    text = 'Radio A',
+    value = 'A',
+    command = rad_func,
+    variable = radio_var)
 radio1.pack()
 
 radio2 = ttk.Radiobutton(
-	window,
-	text = 'Radio Btn 2',
-	value = 2,
-	variable = radio_var,
-	command = lambda: print(radio_var.get()))
+    window, 
+    text = 'Radio B', 
+    value = 'B',
+    command = rad_func,
+    variable = radio_var)
 radio2.pack()
 
+# Check Button
+check_bool = tk.BooleanVar()
+check = ttk.Checkbutton(
+    window, 
+    text = 'Checkbox',
+    command = lambda: print(radio_var.get()),
+    variable = check_bool)
+check.pack()
 
 # Run
 window.mainloop()
-print('Closed mainloop window')
+print('\n----- Closed mainloop window -----')
