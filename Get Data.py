8d3178d6a9f7e1e89f5d1ce4954adaccee0a4b29
Anshulgada <80207612+Anshulgada@users.noreply.github.com>
2023-04-09 20:21:44 +0530

    Exercise completed

--- Get Data.py
+++ Get Data.py
@@ -8,6 +8,13 @@ def btn_func():
     # Update Label
     label.configure(text = 'Updated text')  # Or use this syntax --> label['text'] = 'Updated text' --> Same thing!
     label['text'] = entry_text
+    entry['state'] = 'disabled'         # Disables the entry widget after one use case
+    # print(label.configure())            # Print attributes of label
+
+def btn_old():
+    label.configure(text = 'Lorem Ipsum')
+    entry['state'] = 'enabled'
+
 
 # Create a window
 window = tk.Tk()
@@ -16,13 +23,16 @@ window.geometry('600x300')
 
 # Widgets
 label = ttk.Label(master = window, text = 'Lorem Ipsum')
-label.pack()
+label.pack(pady = 5)
 
 entry = ttk.Entry(master = window)
-entry.pack()
+entry.pack(pady = 5)
 
 button = ttk.Button(master = window, text = 'The button', command = btn_func) 
-button.pack()
+button.pack(pady = 3)
+
+button_back = ttk.Button(master = window, text = 'To old text', command = btn_old) 
+button_back.pack(pady = 0)
 
 # Run
 window.mainloop()
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,2 @@
+ton(master = window, text = 'The button', command = btn_old) 
+# button_back.pack()
